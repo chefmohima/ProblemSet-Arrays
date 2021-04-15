60e71b371f3d9120b4482f469df527265725d23c
Mohima <38052670+chefmohima@users.noreply.github.com>
2021-04-16 01:57:51 +0530

    Create DNF.py

---
+++ DNF.py
@@ -0,0 +1,4 @@
+# Dutch National Flag Problem: Given an array of integers A and a pivot, rearrange A in the following order:
+# [Elements less than pivot, elements equal to pivot, elements greater than pivot]
+
+# For example, if A = [5,2,4,4,6,4,4,3] and pivot = 4 -> result = [3,2,4,4,4,4,6,5]
