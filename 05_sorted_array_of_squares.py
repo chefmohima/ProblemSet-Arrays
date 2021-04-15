024aa65eed032c8eb0ae183ed1091a2ad7ee65c1
Mohima <38052670+chefmohima@users.noreply.github.com>
2021-04-16 01:45:42 +0530

    Update 05_sorted_array_of_squares.py

--- 05_sorted_array_of_squares.py
+++ 05_sorted_array_of_squares.py
@@ -1,3 +1,3 @@
-# IC - 
+# IC - Traverse array from both ends
 # Given a sorted array in non-decreasing order, return an array of squares of each number, also in non-decreasing order. For example:
 # [-4,-2,-1,0,3,5] -> [0,1,4,9,16,25]
