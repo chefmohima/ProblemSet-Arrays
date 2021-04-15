9012ab132d8e35ce4069de38abbd5765ce2a112f
Mohima <38052670+chefmohima@users.noreply.github.com>
2021-04-16 01:53:20 +0530

    Create 07_move_zeros_to_start.py

---
+++ 07_move_zeros_to_start.py
@@ -0,0 +1,3 @@
+# IC - Partition array
+# You are given an array of integers. Rearrange the array so that all zeroes are at the beginning of the array.
+# For example, [4,2,0,1,0,3,0] -> [0,0,0,4,1,2,3]
