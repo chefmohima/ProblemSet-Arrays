7bd808d60382fd4de5ea9a9a3e3e90c1bba77368
Mohima <38052670+chefmohima@users.noreply.github.com>
2021-04-16 01:49:13 +0530

    Update and rename 06_find_continuous_subarray_to_sort to 06_find_continuous_subarray_to_sort.py

--- 06_find_continuous_subarray_to_sort.py
+++ 06_find_continuous_subarray_to_sort.py
@@ -1,2 +1,3 @@
+# IC - Traverse array from both ends
 # Given an array of integers, find the continuous subarray, which when sorted,
 # results in the entire array being sorted. For example: A = [0,2,3,1,8,6,9], result is the subarray [2,3,1,8,6]
